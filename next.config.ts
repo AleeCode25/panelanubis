787aa84fb1308ee4400e6f80c3f1ab6a7a6c21c4
laurahora463 <lauraahora4632025@gmail.com>
2026-05-22 16:39:06 -0300

    casi todo 2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Esto desactiva el chequeo de eslint durante el build de Vercel
+    ignoreDuringBuilds: true,
+  },
   typescript: {
+    // También desactiva el chequeo de tipos si te da errores de TS
     ignoreBuildErrors: true,
-  }
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
